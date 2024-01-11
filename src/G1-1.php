b18e0aad2dee980d1d8939ff6b206e8e86223a9b
eiichikurogi2201414 <148735286+eiichikurogi2201414@users.noreply.github.com>
2024-01-11 09:44:19 +0900

    php,cssヘッダーソース入力

--- src/G1-1.php
+++ src/G1-1.php
@@ -1,1 +1,28 @@
-github作成テスト
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/header.css">
+</head>
+<body>
+<div class="body">
+    <div class="header0">
+    <div class="name0">
+      <img src="../img/logo.png" width="900" height="150">
+    </div>
+    <hr>
+        &emsp;&emsp;
+        
+    <?php
+        echo '<div class="name4">';
+        echo '<h2>博多祝いめでた<br><audio controls autoplay controlslist="nodownload" src="../img/iwaimedeta.MP3"></audio></h2>';
+        echo '</div>';
+    ?>
+    <br><br><br>
+    </div>
+    </div>
+    <hr>
+  </body>
